f87760845d51b371837aa8bece40d396aeed7f59
Nilakshi kuldhar <143577649+Kuldharnilakshi@users.noreply.github.com>
2026-03-21 13:09:45 +0530

    Update App.js

--- frontend-original/src/App.js
+++ frontend-original/src/App.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
-import "@tensorflow/tfjs-backend-webgl";
 import "./App.css";
 import ringtone from "./ringtone.mp3";
+
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-backend-webgl";
+import * as speechCommands from "@tensorflow-models/speech-commands";
+
+// set backend
+tf.setBackend("webgl");
 
 import {
   MapContainer,
